befa4cd2954fff50b4f2ca455a1c2b887b14da0e
Yousiph1 <yousiph77@gmail.com>
2022-02-28 18:40:07 +0000

    for the streak

--- constants/Credentials.ts
+++ constants/Credentials.ts
@@ -1,2 +1,2 @@
-export const BASEURL:string = 'http://192.168.42.231:5000/api/'
-export const SOCKETURL:string = "ws://192.168.42.231:5000/api"
+export const BASEURL:string = 'http://127.0.0.1:5000/api/'
+export const SOCKETURL:string = "ws://127.0.0.1:5000/api"
