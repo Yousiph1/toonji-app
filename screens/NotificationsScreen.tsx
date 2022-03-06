d19f191e0d93f9245129b7b63d7e7f1b7e33e350
Yousiph1 <yousiph77@gmail.com>
2022-03-06 20:19:06 +0000

    for the streak

---
+++ screens/NotificationsScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View,Text, StyleSheet} from 'react-native'
+import { ThemedText, ThemedView } from '../components/Themed'
+
+
+const NotificationsScreen: React.FC = () => {
+
+  return (
+    <ThemedView>
+    <ThemedText>Hello notifications </ThemedText>
+    </ThemedView>
+  )
+}
